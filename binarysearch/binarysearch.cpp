a97cd902322aa461c4b45be8531d31c822501391
voltarou <bagusadnan04@gmail.com>
2024-06-14 11:03:49 +0800

    else if

--- binarysearch/binarysearch.cpp
+++ binarysearch/binarysearch.cpp
@@ -44,6 +44,10 @@ public:
 			parent->leftchild = newNode;  // Make the left child of the parent point to the new node
 		}
 
+		else if (element > parent->info) {  // If the value in the data field of the new node is greater than the value in the data field of the parent node
+			parent->rightchild = newNode;  // Make the right child of the parent point to the new node
+		}
+	
 		
 		
 		
