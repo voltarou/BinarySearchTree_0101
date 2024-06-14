d63d5a18ced3e8368832781ef7e8d36add3167af
voltarou <bagusadnan04@gmail.com>
2024-06-14 11:09:20 +0800

    void postorder

--- binarysearch/binarysearch.cpp
+++ binarysearch/binarysearch.cpp
@@ -93,5 +93,17 @@ public:
 			preorder(ptr->rightchild); // right
 		}
 	}
+	void postorder(Node* ptr) // function to perform inorder traversal
+	{
+		if (ROOT == NULL) {
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL) {
+			postorder(ptr->leftchild); // left
+			postorder(ptr->rightchild); // right
+			cout << ptr->info << " "; // root
+		}
+	}
 };
 
