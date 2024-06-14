aa946f80f6d7c04e9dbdb17f57d32a3744554607
voltarou <bagusadnan04@gmail.com>
2024-06-14 11:03:04 +0800

    if parent

--- binarysearch/binarysearch.cpp
+++ binarysearch/binarysearch.cpp
@@ -35,6 +35,11 @@ public:
 
 		(element, parent, currentNode);   // Locate the node which will be the parent of the new node
 
+		if (parent == nullptr) {  // If the parent is NULL (Tree is empty)
+			ROOT = newNode;  // mark the new node as the root
+			return;  // exit
+		}
+
 		
 		
 		
