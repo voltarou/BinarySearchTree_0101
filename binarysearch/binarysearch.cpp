21b73a1f23f67178d726766bf0dab069088e01b3
voltarou <bagusadnan04@gmail.com>
2024-06-14 11:08:26 +0800

    void inorder

--- binarysearch/binarysearch.cpp
+++ binarysearch/binarysearch.cpp
@@ -69,5 +69,17 @@ public:
 			}
 		}
 	}
+	void inorder(Node* ptr) // function to perform inorder traversal
+	{
+		if (ROOT == NULL) {
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL) {
+			inorder(ptr->leftchild); // left
+			cout << ptr->info << " "; // root
+			inorder(ptr->rightchild); // right
+		}
+	}
 };
 
