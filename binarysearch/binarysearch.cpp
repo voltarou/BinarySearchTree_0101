bd1f87f31c212daa243f6731d0cf69da89c3b0e3
voltarou <bagusadnan04@gmail.com>
2024-06-14 11:10:27 +0800

    switch

--- binarysearch/binarysearch.cpp
+++ binarysearch/binarysearch.cpp
@@ -123,4 +123,39 @@ int main()
 		char ch;
 		cin >> ch;
 		cout << endl;
+		
+		switch (ch)
+		{
+		case '1':
+		{
+			cout << "Enter a word: ";
+			string word;
+			cin >> word;
+			x.insert(word);
+			break;
+		}
+		case '2':
+		{
+			x.inorder(x.ROOT);
+			break;
+		}
+		case '3':
+		{
+			x.preorder(x.ROOT);
+			break;
+		}
+		case '4':
+		{
+			x.postorder(x.ROOT);
+			break;
+		}
+		case '5':
+			return 0;
+		default:
+		{
+			cout << "Invalid option" << endl;
+			break;
+		}
+		}
+	}
 	}
