d4af9dba414c53848e0a67d9b9d2a2e83c643ea1
voltarou <bagusadnan04@gmail.com>
2024-06-14 10:41:51 +0800

    class node public

--- binarysearch/binarysearch.cpp
+++ binarysearch/binarysearch.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Node {
+public:
+	string info;
+	Node* leftchild;
+	Node* rightchild;
+	Node(string i, Node* l, Node* r) {
+		info = i;
+		leftchild = l;
+		rightchild = r;
+	}
+};
