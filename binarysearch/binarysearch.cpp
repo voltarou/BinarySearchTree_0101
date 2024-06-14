526bb163f453b6bf0907d057ebb23ba33c3fabc9
voltarou <bagusadnan04@gmail.com>
2024-06-14 11:06:08 +0800

    while

--- binarysearch/binarysearch.cpp
+++ binarysearch/binarysearch.cpp
@@ -56,6 +56,10 @@ public:
 		// This function searches the current Node of the specified Node as well as the currentNode of its parent
 		currentNode = ROOT;
 		parent = NULL;
+		
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{  // While the currentNode is not NULL and the value in the data field of the currentNode is not equal to the value in the data field of the new node
+		}
 	}
 };
 
